5b5223b4663049505b18187348caafbf559d0520
marshmallow0-0 <possibleway23@gmail.com>
2024-02-27 02:29:50 +0900

    feat: 추가 HowToUsePage 구성시 사용되는 데이터 파일

---
+++ src/pages/data.js
@@ -0,0 +1,27 @@
+export const GUIDE_STEPS = [
+    {
+        image: 'img/guide1.png',
+        title: 'First, Prepare Landmark Picture.',
+        description:
+            '가고싶거나 알아보고 싶은 랜드마크 사진을 준비합니다.\n' +
+            '사진 상태가 선명할수록 좋습니다.\n' +
+            '랜드마크 사진 이여야 합니다.'
+    },
+    {
+        image: './img/guide2.png',
+        title: 'Second, Drag and Drop Image on Drop Box',
+        description:
+            '사진을 드랍 박스 안에 드래그 앤 드랍으로 올려놓습니다. Ai가 당신의 사진을 분석하므로 시간이 걸릴 수 있습니다.\n' +
+            'Ai Search 버튼이 회색에서 초록색으로 활성화 됩니다.\n' +
+            'Ai Search 버튼을 클릭하면 결과 화면을 보여줍니다.'
+    },
+    {
+        image: './img/guide3.png',
+        title: 'Last, Show result to Customer',
+        description:
+            '사진을 성공적으로 분석을 완료하였다면, 사용자에게 최대 5가지의 결과를 제공합니다.\n' +
+            '원하는 장소를 결과물에서 찾으셨다면 창을 클릭합니다.\n' +
+            '원하는 장소의 더 자세한 정보를 간략하게 보여드립니다.\n' +
+            '이 결과가 만족스럽다면 BookMark 버튼을 통해 편하게 다시 찾을 수 있습니다.'
+    }
+];
